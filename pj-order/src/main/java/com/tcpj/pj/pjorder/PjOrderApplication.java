9da67985f6751496cfba5bf2f027ad982c17b0f2
codeyuyu <284717724@qq.com>
2019-01-22 17:58:49 +0800

    订单服务

--- pj-order/src/main/java/com/tcpj/pj/pjorder/PjOrderApplication.java
+++ pj-order/src/main/java/com/tcpj/pj/pjorder/PjOrderApplication.java
@@ -1,13 +1,28 @@
 package com.tcpj.pj.pjorder;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class PjOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PjOrderApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
     }
 
 }
