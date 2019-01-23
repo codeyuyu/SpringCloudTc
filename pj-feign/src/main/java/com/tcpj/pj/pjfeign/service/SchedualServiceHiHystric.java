da66fff6a9f69e1224d4e5c2c90ae67d176ca468
codeyuyu <284717724@qq.com>
2019-01-23 17:39:28 +0800

    断路器

---
+++ pj-feign/src/main/java/com/tcpj/pj/pjfeign/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.tcpj.pj.pjfeign.service;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
