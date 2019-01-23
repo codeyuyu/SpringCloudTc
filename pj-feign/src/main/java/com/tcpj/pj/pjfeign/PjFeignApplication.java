efc1c96ea75684792d12b422d51ca3078df54c7a
codeyuyu <284717724@qq.com>
2019-01-23 11:46:42 +0800

    Feign

---
+++ pj-feign/src/main/java/com/tcpj/pj/pjfeign/PjFeignApplication.java
@@ -0,0 +1,14 @@
+package com.tcpj.pj.pjfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PjFeignApplication.class, args);
+    }
+
+}
+
---
+++ pj-feign/src/test/java/com/tcpj/pj/pjfeign/PjFeignApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tcpj.pj.pjfeign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PjFeignApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
