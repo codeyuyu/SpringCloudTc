94a20a49630831e16adff5cc10d048f499ab127f
codeyuyu <284717724@qq.com>
2019-01-23 16:05:02 +0800

    添加断路器

--- pj-feign/src/main/java/com/tcpj/pj/pjfeign/PjFeignApplication.java
+++ pj-feign/src/main/java/com/tcpj/pj/pjfeign/PjFeignApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +15,7 @@ public class PjFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(PjFeignApplication.class, args);
     }
+
 
 }
 
