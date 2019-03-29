71a636ffe66159eb46f41d8c7f88b64997c690e8
yu <codeyuyu@gmail.com>
2019-03-29 11:38:55 +0800

    骨架工程

--- gateway/src/main/java/org/cy/sc/GatewayServerApplication.java
+++ gateway/src/main/java/org/cy/sc/GatewayServerApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @author xujin
- */
+
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayServerApplication {
